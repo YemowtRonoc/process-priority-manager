efb2790a72747ea821d4e0e71705c2e9fc6fad24
Conor Twomey <conor.twomey95@gmail.com>
2020-05-26 19:29:39 +0100

    Break down functions for readability

--- src/main.rs
+++ src/main.rs
@@ -1,61 +1,71 @@
 #[path = "windows_process/process.rs"]
 mod process;
 
-#[path = "windows_process/process_info.rs"]
-mod process_info;
-
 use serde_json::Result;
 use serde_json::Value;
 
 mod config;
 
-fn parse_process_list(process_list: &Vec<Value>) {
-    for process in process_list {
-        let process_name_option = process["process_name"].as_str();
-        let priority_str = process["process_priority"].as_str();
+fn parse_priority_level(priority_level_option: Option<&str>) -> u32 {
+    let mut priority: u32 = 0;
 
-        let mut priority: u32 = 0;
+    match priority_level_option {
+        Some(priority_level) => priority = process::enumerate_priority_level(priority_level),
+        None => println!("Could not find priority for current process"),
+    };
 
-        match priority_str {
-            Some(priority_level) => priority = process::enumerate_priority_level(priority_level),
-            None => println!("Could not find priority for current process"),
-        };
+    priority
+}
+
+fn parse_process_name(process_name_option: Option<&str>) -> std::string::String {
+    let mut process_name: &str = "";
+    match process_name_option {
+        Some(process_name_str) => process_name = process_name_str,
+        None => println!("Invalid process name received"),
+    };
+
+    if process_name.is_empty() {
+        println!("Process name is empty");
+    }
+
+    process_name.to_string()
+}
+
+fn set_priority(process_name: &str, priority: u32) {
+    let process_id = process::set_cpu_priority_for_process(process_name, priority);
+
+    if process_id != 0 {
+        println!(
+            "Process {} with id ({}) set to priority: {}",
+            process_name,
+            process_id,
+            priority //_str.unwrap()
+        );
+    } else {
+        println!(
+            "ERROR: Process {} failed to set priority: {}",
+            process_name,
+            priority //_str.unwrap()
+        );
+    }
+}
+
+fn set_priority_for_each_process_in_list(process_list: &Vec<Value>) {
+    for current_process in process_list {
+        let process_name_option = current_process["process_name"].as_str();
+        let priority_str = current_process["process_priority"].as_str();
+
+        let priority: u32 = parse_priority_level(priority_str);
 
         if priority == 0 {
-            println!("Not priority set for current process");
+            println!("No priority set for current process");
             continue;
         } else {
-            let mut process_name: &str = "";
-            match process_name_option {
-                Some(process_name_str) => process_name = process_name_str,
-                None => println!("Invalid process name received"),
-            };
-
-            if process_name.is_empty() {
-                println!("Process name is empty");
-            } else {
-                let process_id = process::set_cpu_priority_for_process(process_name, priority);
-
-                if process_id != 0 {
-                    println!(
-                        "Process {} with id ({}) set to priority: {}",
-                        process_name,
-                        process_id,
-                        priority_str.unwrap()
-                    );
-                } else {
-                    println!(
-                        "ERROR: Process {} failed to set priority: {}",
-                        process_name,
-                        priority_str.unwrap()
-                    );
-                }
+            let process_name = parse_process_name(process_name_option);
+            if !process_name.is_empty() {
+                set_priority(&process_name, priority);
             }
         }
-
-        // convert priority_str to priority_const (u32)
-
-        // call set_cpu_priority_for_process
     }
 }
 
@@ -74,9 +84,9 @@ fn main() -> Result<()> {
 
     match process_list_option {
         Some(process_list) => {
-            parse_process_list(process_list);
+            set_priority_for_each_process_in_list(process_list);
         }
-        None => println!("No processes returned from configuration reading"),
+        None => println!("No processes returned from configuration"),
     };
 
     Ok(())
